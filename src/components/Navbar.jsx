72c92c7c150ca99492352e8373266ca72e3fb90c
Sieur Loic <loic.tramis@gmail.com>
2024-06-06 17:06:15 +0200

    Init

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,14 @@
-import React from "react";
 import "./NavBar.css";
 function Navbar() {
-  return (
-    <nav className="Navbar">
-      <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Search</li>
-      </ul>
-    </nav>
-  );
+    return (
+        <nav className="Navbar">
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Search</li>
+            </ul>
+        </nav>
+    );
 }
 
 export default Navbar;
