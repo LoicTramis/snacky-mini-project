fcb18a8d50e8ef1b80d86351758dd0dace501c67
Mark Landers <alexandre@dashlane.com>
2024-06-06 16:21:49 +0200

    resolved conflict

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 import HomePage from "./components/HomePage";
 
 function App() {
-  return <HomePage />;
+  /*
+    For prep time 5min base time + 10 sec per ingredient rounded  (+ random between 1 and 5 min ?)
+  */
+  return (
+    <>
+      <Header />
+      <HomePage />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
