0983782c49470c8ab4ae6258e4cf0bc5cf0579d8
Sieur Loic <loic.tramis@gmail.com>
2024-06-11 15:46:09 +0200

    Finish: about page

--- src/Pages/AboutPage.jsx
+++ src/Pages/AboutPage.jsx
@@ -8,57 +8,64 @@ function AboutPage() {
                 <article className="about-card">
                     <h3>Alexande Tomolillo</h3>
                     <section className="about-meal">
-                        <div className="col">
-                            <img
-                                src="https://familystylefood.com/wp-content/uploads/2021/12/Coq-Au-Vin-hero-macro-500x500.jpg"
-                                width={64}
-                                alt=""
-                            />
-                            <div>
-                                <h4>Favorite meal</h4>
-                                <p>Coq au vin</p>
-                                <p>A chicken braised in red wine, lardons and mushrooms</p>
-                            </div>
-                        </div>
-                        <h4>Favorite dessert</h4>
-                        <div className="col">
-                            <img
-                                src="https://img.cuisineaz.com/660x660/2014/08/07/i31454-photo-de-creme-brulee.jpeg"
-                                width={64}
-                                alt=""
-                            />
-                            <div>
-                                <p>A custard dessert with hard caramel top</p>
-                                <p>Crème brûlée</p>
-                            </div>
-                        </div>
+                        <p>Programming is only a way to show my passion about cooking delicious meal and sharing special tips to make food taste better.</p>
+                        <ul>
+                            <li>
+                                <a
+                                    href="https://github.com/marklanders"
+                                    target="_blank">
+                                    <img
+                                        src="https://cdn.pixabay.com/photo/2022/01/30/13/33/github-6980894_1280.png"
+                                        width={64}
+                                    />
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    href="https://www.linkedin.com/in/alexandre-tomolillo-713113118"
+                                    target="_blank">
+                                    <img
+                                        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmgV3rvl_AvDgG9o7p9b_b1sb1ZVChFrvuNQ&s"
+                                        width={64}
+                                    />
+                                </a>
+                            </li>
+                        </ul>
+                        <p>
+                            Favorite dishes: <em>Coq au vin</em> &bull; <em>Crème brûlée</em>
+                        </p>
                     </section>
                 </article>
                 <article className="about-card">
                     <h3>Lo&iuml;c Tramis</h3>
+
                     <section className="about-meal">
-                        <h4>Favorite meal</h4>
-                        <img
-                            src="https://s3-eu-west-1.amazonaws.com/images-ca-1-0-1-eu/recipe_photos/original/1039/tartiflette-recette-AdobeStock_224565159.jpeg"
-                            width={64}
-                            alt=""
-                        />
-                        <div>
-                            <p>A Savoyard gratin with potatoes, Reblochon cheese, cream and pork</p>
-                            <p>Tartiflette</p>
-                        </div>
-                    </section>
-                    <section className="about-meal">
-                        <h4>Favorite dessert</h4>
-                        <img
-                            src="https://www.cookandrecord.com/wp-content/uploads/2020/08/recette-fraisier-cook-and-record-4-scaled.jpg"
-                            width={64}
-                            alt=""
-                        />
-                        <div>
-                            <p>A cake made of an almond sponge cake, meringue, pastry cream, and strawberries</p>
-                            <p>Fraisier</p>
-                        </div>
+                        <p>I love coding as much as cooking, and gathering recipes from all over the world has never been this much fun.</p>
+                        <ul>
+                            <li>
+                                <a
+                                    href="https://github.com/LoicTramis/"
+                                    target="_blank">
+                                    <img
+                                        src="https://cdn.pixabay.com/photo/2022/01/30/13/33/github-6980894_1280.png"
+                                        width={64}
+                                    />
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    href="https://www.linkedin.com/in/lo%C3%AFc-tramis-274295153/"
+                                    target="_blank">
+                                    <img
+                                        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmgV3rvl_AvDgG9o7p9b_b1sb1ZVChFrvuNQ&s"
+                                        width={64}
+                                    />
+                                </a>
+                            </li>
+                        </ul>
+                        <p>
+                            Favorite dishes: <em>Tartiflette</em> &bull; <em>Fraisier</em>
+                        </p>
                     </section>
                 </article>
             </main>
